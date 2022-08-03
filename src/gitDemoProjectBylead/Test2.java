7f06ec70ceb4b09f09434f84402eed2a2e8b9760
hanumant <hemantguys@gmail.com>
2022-08-03 10:26:37 +0530

    DOne form eamnt

--- src/gitDemoProjectBylead/Test2.java
+++ src/gitDemoProjectBylead/Test2.java
@@ -5,6 +5,10 @@ public class Test2
 	public void Abc2()
 	{
 		System.out.println("This is project by lead");
+System.out.println("This is project by lead");
+System.out.println("This is project by lead");
+System.out.println("This is project by lead");
+System.out.println("This is project by lead");
 	}
 
 }
